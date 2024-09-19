7e5e749e2583806b38ee7f1bb0fa10da2743195f
SvetlanaTodorova28 <svetlana.todorova@student.howest.be>
2024-09-19 18:21:53 +0200

    Add mock pages

--- src/Mde.Project.Mobile/Pages/AboutPage.xaml.cs
+++ src/Mde.Project.Mobile/Pages/AboutPage.xaml.cs
@@ -10,4 +10,24 @@ public partial class AboutPage : ContentPage{
     public AboutPage(){
         InitializeComponent();
     }
+    private async void LabelTapped(object sender, EventArgs e)
+    {
+        var label = sender as Label;
+        if (label != null)
+        {
+            var url = label.GestureRecognizers.OfType<TapGestureRecognizer>().FirstOrDefault()?.CommandParameter as string;
+            if (!string.IsNullOrEmpty(url))
+            {
+                try
+                {
+                    await Launcher.OpenAsync(new Uri(url));
+                }
+                catch (Exception ex)
+                {
+                    
+                    Console.WriteLine("Cannot open URL: " + ex.Message);
+                }
+            }
+        }
+    }
 }
