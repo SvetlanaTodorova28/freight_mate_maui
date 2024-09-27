40feb99cb799101470db338039b852a7736ec018
SvetlanaTodorova28 <svetlana.todorova@student.howest.be>
2024-09-27 16:07:08 +0200

    Add allerts for crud cargo

--- src/Mde.Project.Mobile/Pages/CargoInfoPage.xaml.cs
+++ src/Mde.Project.Mobile/Pages/CargoInfoPage.xaml.cs
@@ -1,13 +1,52 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using Font = Microsoft.Maui.Font;
+
 
 namespace Mde.Project.Mobile.Pages;
 
 public partial class CargoInfoPage : ContentPage{
     public CargoInfoPage(){
         InitializeComponent();
+    }
+    private async void btnDelete_Clicked(object? sender, EventArgs e){
+
+        var snackbarOptions = new SnackbarOptions{
+            BackgroundColor = Colors.Red,
+            TextColor = Colors.White,
+            CornerRadius = new CornerRadius(10),
+            Font = Font.SystemFontOfSize(16),
+            CharacterSpacing = 1,
+            ActionButtonTextColor = Colors.White
+        };
+
+        string text = "Are you sure you want to delete";
+        TimeSpan duration = TimeSpan.FromSeconds(5);
+        Action buttonAction = async () => await DisplayAlert("You chose to delete the cargo", "Are you sure you want to delete this cargo", "Yes");
+         
+        string submit = "Delete the cargo";
+
+        var snackbar = Snackbar.Make(text, buttonAction, submit, duration, snackbarOptions);
+        await snackbar.Show();
+    }
+    private async void btnUpdate_Clicked(object? sender, EventArgs e){
+
+        var snackbarOptions = new SnackbarOptions{
+            BackgroundColor = Colors.DarkOrange,
+            TextColor = Colors.White,
+            CornerRadius = new CornerRadius(10),
+            Font = Font.SystemFontOfSize(16),
+            CharacterSpacing = 1,
+            ActionButtonTextColor = Colors.White
+        };
+
+        string text = "Are you sure you want to delete";
+        TimeSpan duration = TimeSpan.FromSeconds(15);
+        Action buttonAction = async () => await DisplayAlert("You chose to update the cargo", "Are you sure you want to update this cargo", "Yes");
+         
+        string submit = "Update the cargo";
+
+        var snackbar = Snackbar.Make(text, buttonAction, submit, duration, snackbarOptions);
+        await snackbar.Show();
     }
 }
--- src/Mde.Project.Mobile/Pages/CreateCargoPage.xaml.cs
+++ src/Mde.Project.Mobile/Pages/CreateCargoPage.xaml.cs
@@ -3,12 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Maui.Alerts;
+using Font = Microsoft.Maui.Font;
+using CommunityToolkit.Maui.Core;
 using Microsoft.Maui.Controls;
+
 
 namespace Mde.Project.Mobile.Pages;
 
 public partial class CreateCargoPage : ContentPage{
     public CreateCargoPage(){
         InitializeComponent();
+    }
+
+    private async void btnCreate_Clicked(object? sender, EventArgs e){
+
+        var snackbarOptions = new SnackbarOptions{
+            BackgroundColor = Colors.DarkSeaGreen,
+            TextColor = Colors.White,
+            CornerRadius = new CornerRadius(10),
+            Font = Font.SystemFontOfSize(18),
+            CharacterSpacing = 1
+            
+        };
+
+        string text = "CARGO CREATED SUCCESSFULLY 📦";
+        TimeSpan duration = TimeSpan.FromSeconds(5);
+        
+
+        var snackbar = Snackbar.Make(text, null, "", duration, snackbarOptions);
+        await snackbar.Show();
     }
 }
