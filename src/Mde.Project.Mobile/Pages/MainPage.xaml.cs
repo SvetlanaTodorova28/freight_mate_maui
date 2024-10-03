a4239e3fcece4d774ef545cabc1ee931f8cfe7d8
SvetlanaTodorova28 <svetlana.todorova@student.howest.be>
2024-10-03 13:23:57 +0200

    Fix main page buttons in android

--- src/Mde.Project.Mobile/Pages/MainPage.xaml.cs
+++ src/Mde.Project.Mobile/Pages/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Mde.Project.Mobile.Core.Service;
+
 using Mde.Project.Mobile.Core.Service.Interfaces;
 using Mde.Project.Mobile.ViewModels;
 
