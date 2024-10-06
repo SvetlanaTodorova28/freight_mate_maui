26ecf63697c2a875b30db1bb26187a87da2b5df7
SvetlanaTodorova28 <svetlana.todorova@student.howest.be>
2024-10-06 22:19:26 +0200

    Add api project

---
+++ src/Mde.Project.Mobile.WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,5 @@
+namespace Mde.Project.Mobile.WebAPI.Data;
+
+public class ApplicationDbContext{
+    
+}
---
+++ src/Mde.Project.Mobile.WebAPI/Entities/Product.cs
@@ -0,0 +1,5 @@
+namespace Mde.Project.Mobile.WebAPI.Entities;
+
+public class Products{
+    
+}
