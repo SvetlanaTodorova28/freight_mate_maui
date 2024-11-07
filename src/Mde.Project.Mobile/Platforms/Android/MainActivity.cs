231164c7a987b2663ae5bc3818b29ade0080adf4
SvetlanaTodorova28 <svetlana.todorova@student.howest.be>
2024-11-07 14:52:22 +0100

    Add channel for pop to android

--- src/Mde.Project.Mobile/Platforms/Android/MainActivity.cs
+++ src/Mde.Project.Mobile/Platforms/Android/MainActivity.cs
@@ -1,36 +1,62 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
+using Android.Graphics;
 using Android.OS;
-
+using Android.Support.V4.App;
+using AndroidX.Core.App;
+using Microsoft.Maui;
+using Microsoft.Maui.Platform;
+using Color = Android.Graphics.Color;
 
 namespace Mde.Project.Mobile.Platforms;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity{
-    
+public class MainActivity : MauiAppCompatActivity
+{
     protected override void OnCreate(Bundle savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
         CreateNotificationChannel();
     }
+
     private void CreateNotificationChannel()
     {
         if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
         {
-            var channelName = "General Notifications";
-            var channelId = "default_channel";
-            var channelDescription = "General notifications";
-            var importance = NotificationImportance.Default;
-            var notificationChannel = new NotificationChannel(channelId, channelName, importance)
+            string channelName = "General Notifications";
+            string channelId = "default_channel";
+            string channelDescription = "General notifications";
+            NotificationImportance importance = NotificationImportance.High;
+
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance)
             {
-                Description = channelDescription
+                Description = channelDescription,
+                LightColor = Color.Beige
+               
+                
+                
             };
 
-            var notificationManager = (NotificationManager) GetSystemService(NotificationService);
-            notificationManager.CreateNotificationChannel(notificationChannel);
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 
+    public void SendNotification(string messageBody, Context context)
+    {
+        var channelId = "default_channel";
+
+        var notificationBuilder = new NotificationCompat.Builder(context, channelId)
+            .SetSmallIcon(Resource.Drawable.cargos)  // Zorg dat deze resource bestaat in je Drawable map
+            .SetContentTitle("Nieuwe Melding")
+            .SetContentText(messageBody)
+            .SetAutoCancel(true)
+            .SetVisibility(NotificationCompat.VisibilityPublic); // Voor heads-up notificatie
+
+        var notificationManager = NotificationManagerCompat.From(context);
+        notificationManager.Notify(0, notificationBuilder.Build());
+    }
 }
