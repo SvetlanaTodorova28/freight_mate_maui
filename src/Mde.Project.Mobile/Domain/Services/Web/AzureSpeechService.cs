9e28356751f46d9160da5d653de38dd8d7fefc1e
SvetlanaTodorova28 <svetlana.todorova@student.howest.be>
2024-11-09 21:01:43 +0100

    Add interface for speech

---
+++ src/Mde.Project.Mobile/Domain/Services/Web/AzureSpeechService.cs
@@ -0,0 +1,22 @@
+using Mde.Project.Mobile.Domain.Services.Interfaces;
+using Microsoft.CognitiveServices.Speech;
+
+namespace Mde.Project.Mobile.Domain.Services.Web;
+
+public class AzureSpeechService: ISpeechService{
+    public async Task<string> RecognizeSpeechAsync(){
+        var config = SpeechConfig.FromSubscription("<jouw-sleutel>", "northeurope");
+        using (var recognizer = new SpeechRecognizer(config))
+        {
+            var result = await recognizer.RecognizeOnceAsync();
+            if (result.Reason == ResultReason.RecognizedSpeech)
+            {
+                return result.Text;
+            }
+            else
+            {
+                return $"Error: {result.Reason}";
+            }
+        }
+    }
+}
