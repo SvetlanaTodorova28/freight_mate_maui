f6f782bc240d6bacd419b7056a9565e5b2df2167
SvetlanaTodorova28 <svetlana.todorova@student.howest.be>
2024-12-02 13:37:55 +0100

    Start cleaning code

--- src/Mde.Project.Mobile/Domain/Services/Web/Dtos/Categories/CategoryRequestDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Mde.Project.Mobile.Domain.Services.Web.Dtos.Categories;
-
-public class CategoryRequestDto:BaseDto{
-    
-    [Required(ErrorMessage = "{0}  is required")]
-    public string Name { get; set; }
-}
--- src/Mde.Project.Mobile/Domain/Services/Web/Dtos/Categories/CategoryResponseDto.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Mde.Project.Mobile.Domain.Services.Web.Dtos.Categories;
-
-public class CategoryResponseDto:BaseDto{
-    public string Name { get; set; }
-    
-}
